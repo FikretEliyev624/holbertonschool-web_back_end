464bd17d90b27b5875babd9c88a50bd1a5fab2bd
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-10 16:54:25 +0400

    task-11

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" 10-update_topics.py """
+
+def update_topics(mongo_collection, name, topics):
+    """Update the 'topics' field of all documents with the given school name."""
+    if mongo_collection is None or not name or not isinstance(topics, list):
+        return
+    mongo_collection.update_many(
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
